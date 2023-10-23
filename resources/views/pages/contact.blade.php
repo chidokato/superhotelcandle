dc7808a74b13fbb074c6962e7b5d8c4f0c118361
= <=>
2023-10-23 13:58:12 +0700

    sdf

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -35,20 +35,20 @@
     <div class="container">
         <div class="contact_title" style="text-align: center;margin: 0 0 30px 0;">
             <span class="contact_subtitle" style="color:#666666;"></span>
-            <h2 class="contact_title-name" style="color:#111111;font-weight: 400;">Liên hệ</h2>
+            <h2 class="contact_title-name" style="color:#111111;font-weight: 400;">{{__('lang.Contact')}}</h2>
         </div>
         <div class="row" style="justify-content: center;">
             <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-                <form action="/Contact/SubmitContact" method="post" id="contact-form" novalidate="novalidate">
+                <form action="" method="post" id="contact-form" novalidate="novalidate">
                     <div class="contact_form">
                         <input type="hidden" name="Discount" value=": 0%" class="form-control">
-                        <input type="text" required="" name="FullName" class="form-control" placeholder="Tên của bạn" aria-required="true">
-                        <input type="text" required="" name="Tel" class="form-control" placeholder="Số điện thoại" aria-required="true">
-                        <input type="text" required="" name="Email" class="form-control" placeholder="Địa chỉ hòm thư" aria-required="true">
-                        <textarea class="form-control" required="" name="Request" id="mess_contact" rows="10" placeholder="Nội dung" aria-required="true"></textarea>
+                        <input type="text" required name="name" class="form-control" placeholder="{{__('lang.f_name')}}" aria-required="true">
+                        <input type="text" name="phone" class="form-control" placeholder="{{__('lang.f_phone')}}" aria-required="true">
+                        <input type="text" name="email" class="form-control" placeholder="{{__('lang.f_email')}}" aria-required="true">
+                        <textarea class="form-control" name="content" rows="10" placeholder="{{__('lang.f_content')}}" aria-required="true"></textarea>
                         <div class="submit_message">
                             <button class="submit_form">
-                                Gửi Email
+                                {{__('lang.f_cent')}}
                             </button>
                         </div>
                     </div>
